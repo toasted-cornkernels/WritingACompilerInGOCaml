d3ab635e27477d35f1ceb19d0b57d5d523b2d205
toasted-cornkernels <volcano0909@gmail.com>
2023-09-05 21:33:16 -0700

    why does the docstring get appended, not prepended?

--- lib/Lexer.mli
+++ lib/Lexer.mli
@@ -1,15 +1,23 @@
 type t
 
 val read_char : t -> t
+(** Consume a character of the input, shifting it to be the current char. *)
 
 val of_string : String.t -> t
+(** Create a lexer that reads the input string from the beginning. *)
 
 val skip_whitespace : t -> t
+(** Skip the whitespace, and the following ones, that the lexer may currently seeing. *)
 
 val peek_char : t -> Char.t
+(** Peek a character without changing the lexer's current reading position. *)
 
 val read_identifier : t -> t * String.t
+(** Read an identifier starting from the lexer's current position. *)
 
 val read_number : t -> t * String.t
+(** Read a number starting from the lexer's current position. *)
 
 val next_token : t -> t * Token.t
+(** Get the next token which the lexer's current character begins. e.g. Get Operator.Equal or
+    Operator.Assign depending on the peeked character. *)
