947970d1f61d7171d95c5342d4f814e75c2180ed
toasted-cornkernels <volcano0909@gmail.com>
2025-03-22 20:24:11 -0400

    update

--- lib/Parser.ml
+++ lib/Parser.ml
@@ -79,26 +79,33 @@ let peek_token_is (parser : t) (token_type : TokenType.t) : bool =
 
 (** Append a wrong peeked token to the parser's error list. *)
 let peek_error (parser : t) (token_type : Token.TokenType.t) : t =
-  let msg =
+  let error_message =
     F.asprintf "Expected next token to be %s, got %s instead." (TokenType.to_string token_type)
       (TokenType.to_string parser.peek_token.type_)
   in
-  (* TODO: Define a getter that reverses the errors list; just cons everything *)
-  {parser with errors= parser.errors @ [msg]}
+  {parser with errors= error_message :: parser.errors}
 
 
+(** If the parser's peeked token has the expected token type, then advance the parser. Otherwise, do
+    not advance it and instead add an new entry in the parser error. *)
 let expect_peek (parser : t) (token_type : TokenType.t) : t * bool =
   if peek_token_is parser token_type then (next_token parser, true)
   else (peek_error parser token_type, false)
 
 
+let errors (parser : t) : string list = List.rev parser.errors
+
 (* ==================== Expression Parsers ==================== *)
 
 exception Not_An_Integer of string
 
-type prefix_parser = t -> t * AST.Expression.t
+exception Not_A_Boolean of string
 
-type infix_parser = t -> AST.Expression.t -> t * AST.Expression.t
+exception Not_A_Group_Expr
+
+type prefix_parser = t -> t * AST.Expression.t option
+
+type infix_parser = t -> AST.Expression.t -> t * AST.Expression.t option
 
 let rec parse_expression (parser : t) (precendence : precedence) : t * Expression.t =
   let infix_parser = dispatch_prefix_parser parser.current_token.type_ in
@@ -106,10 +113,32 @@ let rec parse_expression (parser : t) (precendence : precedence) : t * Expressio
   raise TODO
 
 
+(** Determine which prefix parser to use when trampolining. *)
+and dispatch_prefix_parser (token_type : TokenType.t) : prefix_parser =
+  let open TokenType in
+  match token_type with
+  | IdentLiteral IdentLiteral.Ident ->
+      parse_identifier
+  | IdentLiteral IdentLiteral.Int ->
+      parse_integer
+  | Operator Operator.Bang | Operator Operator.Minus ->
+      parse_prefix_expression
+  | Keyword Keyword.True | Keyword Keyword.False ->
+      parse_boolean
+  | Delimiter Delimiter.LParen ->
+      parse_grouped_expression
+  | Keyword Keyword.If ->
+      parse_if
+  | Keyword Keyword.Function ->
+      parse_function
+  | _ ->
+      raise @@ No_Prefix_Parser_Available token_type
+
+
 and parse_identifier : prefix_parser =
  fun parser ->
   (* Uhh.. shouldn't we advance the parser here by one token? *)
-  (parser, Identifier {token= parser.current_token; value= parser.current_token.literal})
+  (parser, Some (Identifier {token= parser.current_token; value= parser.current_token.literal}))
 
 
 and parse_integer : prefix_parser =
@@ -117,9 +146,12 @@ and parse_integer : prefix_parser =
   match Int.of_string_opt parser.current_token.literal with
   | Some value ->
       (* Uhh.. shouldn't we advance the parser here by one token? *)
-      (parser, Integer {token= parser.current_token; value})
+      (parser, Some (Integer {token= parser.current_token; value}))
   | None ->
-      raise (Not_An_Integer parser.current_token.literal)
+      let error_message =
+        F.asprintf "Could not parse %s as integer." parser.current_token.literal
+      in
+      ({parser with errors= error_message :: parser.errors}, None)
 
 
 and parse_prefix_expression : prefix_parser =
@@ -129,35 +161,86 @@ and parse_prefix_expression : prefix_parser =
     parse_expression current_operator_consumed_parser Prefix
   in
   ( right_expression_parsed_parser
-  , Prefix {token= parser.current_token; operator= parser.current_token.literal; right= expression}
+  , Some
+      (Prefix
+         {token= parser.current_token; operator= parser.current_token.literal; right= expression} )
   )
 
 
-and parse_boolean : prefix_parser = fun _ -> raise TODO
+and parse_boolean : prefix_parser =
+ fun parser ->
+  let current_token = parser.current_token in
+  match parser.current_token.type_ with
+  | Keyword True ->
+      (parser, Some (Boolean {token= current_token; value= true}))
+  | Keyword False ->
+      (parser, Some (Boolean {token= current_token; value= false}))
+  | _ ->
+      let error_message =
+        F.asprintf "Could not parse token %s as a boolean" parser.current_token.literal
+      in
+      ({parser with errors= error_message :: parser.errors}, None)
 
-and parse_grouped_expression : prefix_parser = fun _ -> raise TODO
 
-and parse_if : prefix_parser = fun _ -> raise TODO
+and parse_grouped_expression : prefix_parser =
+ fun parser ->
+  let expression_parsed_parser, expression = parse_expression parser Lowest in
+  match expect_peek expression_parsed_parser (Delimiter RParen) with
+  | rparen_consumed_parser, true ->
+      (rparen_consumed_parser, Some expression)
+  | error_parser, false ->
+      (error_parser, None)
+
+
+and parse_if : prefix_parser =
+ fun parser ->
+  let if_token = parser.current_token in
+  match expect_peek parser (Delimiter LParen) with
+  | lparen_consumed_parser, true -> (
+      let expression_parsed_parser, condition_expression =
+        (* If-expressions have the lowest precedence. *)
+        parse_expression lparen_consumed_parser Lowest
+      in
+      match expect_peek expression_parsed_parser (Delimiter RParen) with
+      | rparen_consumed_parser, true -> (
+        match expect_peek rparen_consumed_parser (Delimiter LBrace) with
+        | lbrace_consumed_parser, true -> (
+            let consequence_block_parsed_parser, consequence =
+              parse_block_statement lbrace_consumed_parser
+            in
+            match peek_token_is consequence_block_parsed_parser (Keyword Else) with
+            | true -> (
+              match expect_peek consequence_block_parsed_parser (Delimiter LBrace) with
+              | lbrace_consumed_parser, true ->
+                  let alternative_block_parsed_parser, alternative =
+                    parse_block_statement lbrace_consumed_parser
+                  in
+                  ( alternative_block_parsed_parser
+                  , Some
+                      (If
+                         { token= if_token
+                         ; condition= condition_expression
+                         ; then_= consequence
+                         ; else_= Some alternative } ) )
+              | error_parser, false ->
+                  (error_parser, None) )
+            | false ->
+                ( consequence_block_parsed_parser
+                , Some
+                    (If
+                       { token= if_token
+                       ; condition= condition_expression
+                       ; then_= consequence
+                       ; else_= None } ) ) )
+        | error_parser, false ->
+            (error_parser, None) )
+      | error_parser, false ->
+          (error_parser, None) )
+  | error_parser, false ->
+      (error_parser, None)
+
 
 and parse_function : prefix_parser = fun _ -> raise TODO
-
-and parse_plus : infix_parser = fun _ _ -> raise TODO
-
-and parse_infix_minus : infix_parser = fun _ _ -> raise TODO
-
-and parse_slash : infix_parser = fun _ _ -> raise TODO
-
-and parse_asterisk : infix_parser = fun _ _ -> raise TODO
-
-and parse_equal : infix_parser = fun _ _ -> raise TODO
-
-and parse_not_equal : infix_parser = fun _ _ -> raise TODO
-
-and parse_less_than : infix_parser = fun _ _ -> raise TODO
-
-and parse_greater_than : infix_parser = fun _ _ -> raise TODO
-
-and parse_infix_lparen : infix_parser = fun _ _ -> raise TODO
 
 (** Determine which infix parser to use when trampolining. *)
 and dispatch_infix_parser (token_type : TokenType.t) : infix_parser =
@@ -184,27 +267,23 @@ and dispatch_infix_parser (token_type : TokenType.t) : infix_parser =
       raise @@ No_Infix_Parser_Available token_type
 
 
-(** Determine which prefix parser to use when trampolining. *)
-and dispatch_prefix_parser (token_type : TokenType.t) : prefix_parser =
-  let open TokenType in
-  match token_type with
-  | IdentLiteral IdentLiteral.Ident ->
-      parse_identifier
-  | IdentLiteral IdentLiteral.Int ->
-      parse_integer
-  | Operator Operator.Bang | Operator Operator.Minus ->
-      parse_prefix_expression
-  | Keyword Keyword.True | Keyword Keyword.False ->
-      parse_boolean
-  | Delimiter Delimiter.LParen ->
-      parse_grouped_expression
-  | Keyword Keyword.If ->
-      parse_if
-  | Keyword Keyword.Function ->
-      parse_function
-  | _ ->
-      raise @@ No_Prefix_Parser_Available token_type
+and parse_plus : infix_parser = fun _ _ -> raise TODO
 
+and parse_infix_minus : infix_parser = fun _ _ -> raise TODO
+
+and parse_slash : infix_parser = fun _ _ -> raise TODO
+
+and parse_asterisk : infix_parser = fun _ _ -> raise TODO
+
+and parse_equal : infix_parser = fun _ _ -> raise TODO
+
+and parse_not_equal : infix_parser = fun _ _ -> raise TODO
+
+and parse_less_than : infix_parser = fun _ _ -> raise TODO
+
+and parse_greater_than : infix_parser = fun _ _ -> raise TODO
+
+and parse_infix_lparen : infix_parser = fun _ _ -> raise TODO
 
 (* ==================== Statement Parsers ==================== *)
 
@@ -258,7 +337,7 @@ let parse_expression_statement (parser : t) : t * ExpressionStatement.t =
     (expression_parsed_parser, {token= current_token; expression= Some expression})
 
 
-let parse_statement (parser : t) : t * Statement.t =
+let rec parse_statement (parser : t) : t * Statement.t =
   match parser.current_token.type_ with
   | Keyword Keyword.Let ->
       let let_statement_parsed_parser, let_statement = parse_let_statement parser in
@@ -271,6 +350,24 @@ let parse_statement (parser : t) : t * Statement.t =
         parse_expression_statement parser
       in
       (expression_statement_parsed_parser, Expression expression_statement)
+
+
+and parse_block_statement (parser : t) : t * BlockStatement.t =
+  let lbrace_token = parser.current_token in
+  let rec inner (current_statements : Statement.t list) (current_parser : t) =
+    (* Normally the loop should halt on hitting an RBrace, but the LBrace may not be properly closed. *)
+    if
+      current_token_is current_parser (Delimiter RBrace)
+      || current_token_is current_parser (Meta EOF)
+    then (current_parser, List.rev current_statements)
+    else
+      (* TODO: The Go code says parse_statement can return nil. Maybe we should turn this function to return an option. *)
+      let statement_parsed_parser, statement = parse_statement current_parser in
+      (* QUESTION: Why do we have to advance the parser when looping? *)
+      inner (statement :: current_statements) (next_token statement_parsed_parser)
+  in
+  let statements_parsed_parser, parsed_statements = inner [] parser in
+  (statements_parsed_parser, {token= lbrace_token; statements= parsed_statements})
 
 
 let parse_program (parser : t) : Program.t =
